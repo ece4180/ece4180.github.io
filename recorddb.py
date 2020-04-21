7c7046427b4f01bb271e3ec2a87c2bf658e38359
JeremyKwok <42374426+JeremyKwok@users.noreply.github.com>
2020-04-22 04:38:06 +0800

    Update recorddb.py
    static and dynamic readings

--- recorddb.py
+++ recorddb.py
@@ -13,18 +13,22 @@ class Geoniometer(object):
         <form method="get" action="measure">
             <input type="text" name="Name"/>
             <button type="submit">Get reading for patient</button>
+            <input type="radio" id="static" name="type" value="static">
+            <label for="static">Static</label><br>
+            <input type="radio" id="dynamic" name="type" value="dynamic">
+            <label for="dynamic">Dynamic</label><br>
         </form>
     </body>
 </html>"""
+
     @cherrypy.expose
-    def measure(self, name="Test Name"):
+    def measure(self, name="Test Name", type="static"):
         # guard clause against malicious input
         name = name.tolower()
         pattern = re.compile("[a-z ]+")
         if !pattern.fullmatch(name):
             return "Use only letters and spaces"
-        adc = Adafruit_ADS1x15.ADS1115()
-        reading = adc.read_adc(0, gain=1)
+        reading = stable_reading() if type == "static" else max_reading()
         dtg = time.strftime('%Y-%m-%d %H:%M:%S, time.localtime())
         print 'Local current time:', dtg
         print 'flex value:', reading
@@ -44,6 +48,25 @@ class Geoniometer(object):
             conn.close()
             
         return "name: " + name + "reading:" str(reading)
+                            
+    def stable_reading(self):
+        adc = Adafruit_ADS1x15.ADS1115()
+        readings = list()
+        for i in range(10):
+            reading = adc.read_adc(0, gain=1)
+            readings.append(reading)
+            time.sleep(0.125)
+        return sum(readings) / len(readings)
+        
+    def max_reading(self):
+        adc = Adafruit_ADS1x15.ADS1115()
+        readings = list()
+        # for 6.25 seconds
+        for i in range(200):
+            reading = adc.read_adc(0, gain=1)
+            readings.append(reading)
+            time.sleep(0.03125)
+        return max(readings)
         
 if __name__ == '__main__':
     cherrypy.quickstart(Goniometer())
